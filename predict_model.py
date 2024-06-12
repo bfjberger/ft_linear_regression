1b0d4bf40e92ac1b227ba4d0a2b6090d6baf8dc1
Benoit Berger <bberger@student.42lausanne.ch>
2024-06-12 11:50:11 +0200

    estimatePrice

---
+++ predict_model.py
@@ -0,0 +1,16 @@
+def estimatePrice(m):
+    m = float(m)
+    a = -0.5
+    b = 20000
+    price = a*m + b 
+    print("Estimated car price in function of mileage is: USD{}".format(price))
+
+print("Please enter a mileage, so we can return a theoritical car price:")
+
+mileage = input()
+
+estimatePrice(mileage)
+
+
+
+
